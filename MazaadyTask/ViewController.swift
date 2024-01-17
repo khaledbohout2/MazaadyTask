064db40c10800ba7f068eca4be8338986859cf87
khaledbohout2 <46874509+khaledbohout2@users.noreply.github.com>
2024-01-18 00:01:47 +0200

    Initial Commit

---
+++ MazaadyTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MazaadyTask
+//
+//  Created by Khaled Bohout on 18/01/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
